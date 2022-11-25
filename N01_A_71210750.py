5b3e1462a8816c09d0ec7b4f0c5eab4c9e1b569a
mariomasahe <118040604+mariomasahe@users.noreply.github.com>
2022-11-25 10:26:56 +0700

    Add files via upload

---
+++ N01_A_71210750.py
@@ -0,0 +1,9 @@
+a = input("masukkan nama mahasiswa: ")
+b = input("masukkan NIM-nya: ")
+c = int (b [2:4])
+d = int (b [0:2])
+
+if c == 20  or c ==21 or c == 22 and d== 71 :
+    print(Nama,"merupakan mahasiswa informatika 20 hingga 22")
+else:
+    print(Nama,"bukan mahasiswa informatika 20 hingga 22")
---
+++ n03_A_71210750.py
@@ -0,0 +1,14 @@
+a = int(input("masukan nilai soal 1: ")
+b = int(input("masukan nilai soal 2: ")
+c = int(input("masukan nulai soal 3: ")
+umur = int(input("masukan umur anda: ")
+
+d = (a*(0.5)+ b*(0.3)+ c*(0.2)
+     print("selamat anda lulus:",d)
+
+if umur > 25 and d >= 90:
+     print("selamat anada lulus")
+elif umur <=25 and d >=80:
+    print("selamat anda lulus")
+else:
+    print("coba lagi tahun depan')
